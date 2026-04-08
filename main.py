c6befac689e80fbee4b64bcb1d7095e4835abe30
Anamika Singh <33391809+anamika03@users.noreply.github.com>
2026-04-08 18:43:28 +0200

    Refactor email and password to use environment variables
    Use environment variables for email and password.

---
+++ main.py
@@ -0,0 +1,27 @@
+import smtplib
+import datetime as dt
+import random
+import os
+
+# import os and use it to get the Github repository secrets
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 2:  # Thursday is represented by 3
+    with open("/Users/anamika/Documents/100_Days_of_Python/100-Days-Of-Python/Day 32/quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls() 
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+        from_addr=MY_EMAIL, 
+        to_addrs=MY_EMAIL, 
+        msg=f"Subject:Monday Motivation\n\n{quote}"
+    )
+print("Email sent successfully!")
+
